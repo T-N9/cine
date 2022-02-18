9b578056719391da8d4c7176d05a1dd40c44b544
T-N9 <tenyainmoelwin@gmail.com>
2022-02-18 23:22:55 +0630

    Update theatre movies

--- src/components/theatre-movies/SlideItem.js
+++ src/components/theatre-movies/SlideItem.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './styles/SlideItem.module.scss';
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { setItemId, setItemType } from '../../redux/detailMovieTVSlice';
+import useFetch  from '../../hooks/useFetch';
 
 const SlideItem = (props) => {
 
     let route_type = 'movies';
+    const [ getData, setGetData ] = useState(null);
 
     const dispatch = useDispatch();
     const getItemInfo = () => {
         dispatch(setItemId(props.id));
         dispatch(setItemType('movie'));
+    }
+
+    const { data } = useFetch(`http://api.themoviedb.org/3/movie/${props.id}?api_key=68d49bbc8d40fff0d6cafaa7bfd48072&append_to_response=videos`)
+
+    useEffect(() => {
+        if(data !== null) {
+            setGetData(data)
+        }
+    }, [data]);
+
+    let tagline;
+
+    if(getData !== null) {
+        tagline = data.tagline;
     }
 
     return (
@@ -23,9 +39,9 @@ const SlideItem = (props) => {
                     <h1 className={`${styles.title} ${styles.title_3}`}>
                         {props.title}
                     </h1>
-                    {/* <p className={styles.overview}>
-                        {props.overview}
-                    </p> */}
+                    <p className={styles.tagline}>
+                        “ {tagline} ”
+                    </p>
                 </div>
             </Link>
         </div>
