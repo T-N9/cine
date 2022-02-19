fd6c8ee96ffd97d82d4a8b1e3ff4863925d7607e
T-N9 <tenyainmoelwin@gmail.com>
2022-02-19 19:42:38 +0630

    Update trending card design issues

--- src/components/slider-com-1/SlideCardItem.js
+++ src/components/slider-com-1/SlideCardItem.js
@@ -7,11 +7,11 @@ import { setItemId, setItemType } from '../../redux/detailMovieTVSlice';
 const SlideCardItem = (props) => {
 
     let vote_border;
-    if(props.vote_avg >= 7.5) {
+    if (props.vote_avg >= 7.5) {
         vote_border = '#21E6C1';
     } else if (props.vote_avg >= 6) {
         vote_border = '#f1d900';
-    } else if (props.vote_avg > 0){
+    } else if (props.vote_avg > 0) {
         vote_border = '#FF304F';
     } else {
         vote_border = '#ffffffb4';
@@ -27,24 +27,25 @@ const SlideCardItem = (props) => {
     }
     return (
         <div className={styles.slide_card_item}>
-            <Link onClick={getItemInfo} to={`/${route_type}/${props.id}`}>
-                <div className={styles.wrapper}>
-                    <img 
-                    src={`https://www.themoviedb.org/t/p/original/${props.image}`} 
-                    alt={`${props.title} poster`} 
-                    className={styles.image}
+
+            <div className={styles.wrapper}>
+                <Link onClick={getItemInfo} to={`/${route_type}/${props.id}`}>
+                    <img
+                        src={`https://www.themoviedb.org/t/p/original/${props.image}`}
+                        alt={`${props.title} poster`}
+                        className={styles.image}
                     />
                     <div className={styles.content}>
                         <h1 className={styles.title}>
                             {props.title ? props.title : props.name}
                         </h1>
-                        <p style={{'borderColor' : vote_border}} className={styles.vote}>
+                        <p style={{ 'borderColor': vote_border }} className={styles.vote}>
                             {props.vote_avg === 0 ? "NR" : props.vote_avg}
                         </p>
-                        {/* <p className={styles.date}>{props.release_date}</p> */}
                     </div>
-                </div>
-            </Link>
+                </Link>
+            </div>
+
         </div>
     );
 }
